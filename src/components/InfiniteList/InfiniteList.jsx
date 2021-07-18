953138006bed52e59ec0e7ece6b93a377cfbaaf3
Maximtamgoo <maximtamgoo@gmail.com>
2021-07-18 01:29:46 -0400

    removed react-intersection-observer

--- src/components/InfiniteList/InfiniteList.jsx
+++ src/components/InfiniteList/InfiniteList.jsx
@@ -1,38 +1,25 @@
 import { useState, useEffect, useCallback, useRef } from "react"
 import useIntersectionObserver from "../../hooks/useIntersectionObserver"
-// import { useInView } from 'react-intersection-observer';
 import style from './InfiniteList.module.css'
 
 export default function InfiniteList({ parentStyle, fetchMore, loader, children }) {
   const pendingRef = useRef(false)
-  // const [pending, setPending] = useState(true)
   const [hasMore, setHasMore] = useState(true)
-
-  // const ref = useRef()
   const [ref, inView] = useIntersectionObserver()
-  // console.log('inView:', inView)
   // const [error, setError] = useState(null)
-  // const { ref, inView } = useInView()
 
   const fetchMoreWrapper = useCallback(async () => {
     try {
       pendingRef.current = true
-      // setPending(true)
       const hasMore = await fetchMore()
       if (!hasMore) {
         setHasMore(hasMore)
       }
       pendingRef.current = false
-      // setPending(false)
     } catch (error) {
       console.log('fetchMoreWrapper error:', error)
     }
   }, [fetchMore])
-
-  // useEffect(() => {
-  //   console.log('useEffect inital fetchMoreWrapper')
-  //   fetchMoreWrapper()
-  // }, [fetchMoreWrapper])
 
   useEffect(() => {
     // console.log('useEffect fetchMoreWrapper')
