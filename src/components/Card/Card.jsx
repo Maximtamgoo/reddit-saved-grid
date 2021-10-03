8e9d6a56ec6e3c6f4b99fe2da04add99ae3ec8bd
Maximtamgoo <maximtamgoo@gmail.com>
2021-10-02 20:44:10 -0400

    added unsave/save functionality

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react'
 import style from './Card.module.css'
-import { ReactComponent as BookmarkIcon } from '../../svg//bookmark.svg';
+import { ReactComponent as BookmarkIcon } from '../../svg//bookmark.svg'
+import { saveContent, unsaveContent } from '../../services/reddit'
+import getCookie from '../../utils/getCookie'
 
 export default function Card({ item }) {
+  const [saved, setSaved] = useState(true)
+
   const authorLink = `https://www.reddit.com/u/${item.author}`
   const subredditLink = `https://www.reddit.com/${item.subreddit}`
   const postLink = `https://www.reddit.com${item.permalink}`
+
+  const onClick = async () => {
+    try {
+      let access_token = getCookie('access_token')
+      if (saved) {
+        await unsaveContent(item.id, access_token)
+        setSaved(false)
+      } else {
+        await saveContent(item.id, access_token)
+        setSaved(true)
+      }
+    } catch (error) {
+      console.log('error:', error)
+    }
+  }
 
   return (
     <div className={style.card}>
@@ -17,8 +37,10 @@ export default function Card({ item }) {
           </div>
           <a className={style.title} href={postLink} target='_blank' rel="noreferrer">{item.title}</a>
         </div>
-        <div className={style.bookmark}>
-          <BookmarkIcon className={style.icon} />
+        <div className={style.bookmark} onClick={onClick}>
+          <BookmarkIcon className={style.icon}
+            style={{fill: (saved) ? 'var(--blue)': 'none' }}
+          />
         </div>
       </div>
 
