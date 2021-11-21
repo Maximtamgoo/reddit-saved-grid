9562f61e9f6641f8f27da427165f1d58f13eaca8
Maximtamgoo <maximtamgoo@gmail.com>
2021-11-21 18:49:06 -0500

    navbar todo added & card shadow

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import style from './Navbar.module.css'
 import useAuth from '../../hooks/useAuth'
+// import { useState, useEffect, useRef } from 'react'
 
 export default function Navbar() {
+  // const [shrink, setShrink] = useState(false)
   const auth = useAuth()
+  // const ref = useRef()
+
+  // TODO shrink or hide navbar on scroll
+  // useEffect(() => {
+  //   window.onscroll = function () {
+  //     console.log('ref.current:', ref.current.offsetTop)
+  //     if (ref.current.offsetTop > 60) {
+  //       setShrink(true)
+  //     } else {
+  //       setShrink(false)
+  //     }
+  //   }
+  // }, [])
+
+  // <div ref={ref} className={`${style.navbar} ${shrink ? style.shrink : ''}`}>
 
   return (
     <div className={style.navbar}>
