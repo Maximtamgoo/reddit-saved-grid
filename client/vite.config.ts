b2eb883c677101fc1797a5982a562e7e37592958
Maximtamgoo <maximtamgoo@gmail.com>
2023-01-04 22:20:17 -0500

    configured preact

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 import preact from '@preact/preset-vite'
 import svgr from 'vite-plugin-svgr'
 import checker from 'vite-plugin-checker'
