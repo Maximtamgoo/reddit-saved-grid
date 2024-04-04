a30a09c6c52cd7ce91cb313a5682747a3ef98c61
Maximtamgoo <maximtamgoo@gmail.com>
2024-04-04 14:39:28 -0400

    more messing with react-virtual

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,34 +1,31 @@
-import { useEffect, useMemo } from "react";
+import { useMemo, useState, useEffect, useCallback, useLayoutEffect, useRef } from "react";
 import { useGetSavedContent } from "../services/queries";
 import { useWindowVirtualizer } from "@tanstack/react-virtual";
-import Card from "../components/Card";
+import Card from "../components/Card/Card";
 import ThreeDots from "../svg/three-dots.svg?react";
 import useWindowWidth from "../hooks/useWindowWidth";
-// import useIntersectionObserver from "../hooks/useIntersectionObserver";
 import { useInView } from "react-intersection-observer";
+import Modal from "../components/Modal/Modal";
+import { Post } from "../schema/Post";
 
 const ITEM_SIZE = 400;
 const MAX_LANES = 3;
-const MIN_LANES = 2;
+const MIN_LANES = 1;
 
 export default function MainPage({ username }: { username: string }) {
-  const width = useWindowWidth();
-  // const targetRef = useRef(null);
-  // const isIntersecting = useIntersectionObserver(targetRef, {
-  //   rootMargin: "75%"
-  //   // threshold: 1
-  // });
-  // console.log("isIntersecting:", isIntersecting);
+  const [isOpen, setIsOpen] = useState(false);
+  const modalDataRef = useRef<Post>();
 
   const { ref, inView } = useInView();
 
-  const { data, isLoading, isError, hasNextPage, fetchNextPage, isFetched } =
+  const { data, isLoading, isError, error, hasNextPage, fetchNextPage, isFetched } =
     useGetSavedContent(username);
-  // console.log("data:", data?.pages[0].posts[1]);
 
   const allItems = useMemo(() => {
     return data?.pages.flatMap((page) => page.posts) ?? [];
   }, [data]);
+
+  const width = useWindowWidth();
 
   const lanes = useMemo(
     () => Math.max(MIN_LANES, Math.min(MAX_LANES, Math.floor(width / ITEM_SIZE))),
@@ -37,68 +34,74 @@ export default function MainPage({ username }: { username: string }) {
 
   const winVirtualizer = useWindowVirtualizer({
     count: allItems.length,
-    // count: hasNextPage ? allItems.length + 1 : allItems.length,
-    estimateSize: () => ITEM_SIZE,
-    overscan: lanes * 2,
-    lanes
+    estimateSize: useCallback(() => ITEM_SIZE, []),
+    overscan: 20,
+    lanes,
+    getItemKey: useCallback((index: number) => allItems[index].id, [allItems])
   });
+
+  useLayoutEffect(() => {
+    if (lanes) winVirtualizer.measure();
+  }, [lanes, winVirtualizer]);
+
+  const onClickPreview = useCallback((post: Post) => {
+    modalDataRef.current = post;
+    setIsOpen(true);
+  }, []);
 
   useEffect(() => {
     if (inView) fetchNextPage();
   }, [inView, fetchNextPage]);
 
-  if (isError) return <div>isError main</div>;
-  // if (isPending) return <div>isPending main</div>;
+  if (isError) {
+    console.log("error:", error);
+    return <div>{error}</div>;
+  }
 
   if (isLoading) {
     return (
-      <main className="absolute inset-0 top-16 grid place-items-center bg-zinc-900">
+      <main className="absolute inset-0 grid place-items-center bg-zinc-900">
         <ThreeDots className="fill-blue-500" />
       </main>
     );
   }
 
-  // const maxWidth = Math.floor(100 / (width / (ITEM_SIZE * MAX_LANES)));
-  // console.log("maxWidth:", maxWidth);
-
   return (
     <main className="bg-zinc-900 text-blue-500">
+      {isOpen && modalDataRef.current && (
+        <Modal post={modalDataRef.current} onClose={() => setIsOpen(false)} />
+      )}
       <div
+        className="relative mx-auto"
         style={{
           height: `${winVirtualizer.getTotalSize()}px`,
-          margin: "0 auto",
-          maxWidth: `${ITEM_SIZE * MAX_LANES}px`,
-          // maxWidth: `${maxWidth}%`,
-          position: "relative"
+          maxWidth: lanes < MAX_LANES ? "100%" : "90%"
         }}
       >
-        {winVirtualizer.getVirtualItems().map(({ index, lane, start }) => {
-          const post = allItems[index];
+        {winVirtualizer.getVirtualItems().map(({ index, lane, start, key }) => {
+          const item = allItems[index];
           const percent = 100 / lanes;
-          // const isLoaderItem = index > allItems.length - 1;
 
           return (
             <div
-              className="p-1.5"
-              key={post.id}
+              className="absolute top-0 p-2"
+              key={key}
               ref={winVirtualizer.measureElement}
               data-index={index}
               style={{
-                position: "absolute",
-                top: 0,
                 left: `${lane * percent}%`,
                 width: `${percent}%`,
                 transform: `translateY(${start}px)`
               }}
             >
-              <Card post={post} />
+              <Card post={item} onClickPreview={onClickPreview} />
             </div>
           );
         })}
       </div>
       {isFetched && (
-        <div ref={ref} className="grid h-20 place-items-center">
-          {hasNextPage ? <ThreeDots className="fill-blue-500" /> : "The End"}
+        <div ref={ref} className="grid h-20 place-items-center text-lg">
+          {hasNextPage ? <ThreeDots className="fill-blue-500" /> : "The End?"}
         </div>
       )}
     </main>
