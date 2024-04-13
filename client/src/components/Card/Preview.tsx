c159af759d6baa0e7763b179c8eaa4691eea0227
Maximtamgoo <maximtamgoo@gmail.com>
2024-04-13 19:00:54 -0400

    add visibility style

--- client/src/components/Card/Preview.tsx
+++ client/src/components/Card/Preview.tsx
@@ -15,25 +15,27 @@ export default function Preview({ url, onClick, children }: PropsWithChildren<Pr
     setLoading(false);
   }
 
-  if (!url || isError) {
-    return <div className="grid aspect-square place-items-center rounded-md text-8xl">?</div>;
+  if (isError) {
+    return <div className="grid aspect-square place-items-center bg-zinc-800 text-8xl">?</div>;
   }
 
   return (
     <div
-      className="relative cursor-pointer overflow-hidden rounded-md bg-zinc-700"
+      className="relative cursor-pointer overflow-hidden rounded-b-xl border-4 border-zinc-800 bg-zinc-700"
       onClick={onClick}
     >
       <img className="absolute h-full w-full object-cover opacity-40 blur-xl" src={url} />
       <img
         className="relative m-auto object-contain"
+        loading="lazy"
         src={url}
         onLoad={onLoad}
         onError={() => setIsError(true)}
         alt="Reddit Content"
         style={{
           minHeight: "300px",
-          maxHeight: "calc(100vh - 14rem)"
+          maxHeight: "calc(100vh - 14rem)",
+          visibility: loading ? "hidden" : "visible"
         }}
       />
       {loading && (
