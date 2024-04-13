46179b3495442f228dfafc44b2321e64d892caed
Maximtamgoo <maximtamgoo@gmail.com>
2024-04-13 13:55:20 -0400

    corner info refactor

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -18,33 +18,33 @@ export default function Card({ post, onClickPreview }: Props) {
   if (post.type === "gallery") {
     Post = (
       <Preview url={post.preview} onClick={() => onClickPreview(post)}>
-        {post.gallery.length > 1 && (
-          <div className="absolute right-2 top-2 grid h-10 min-w-10 place-items-center rounded-md bg-zinc-900 px-2 text-lg">
-            {post.gallery.length}
-          </div>
-        )}
+        {post.gallery.length > 1 && <CornerInfo data={post.gallery.length} />}
       </Preview>
     );
   } else if (post.type === "image") {
     Post = (
       <Preview url={post.preview} onClick={() => onClickPreview(post)}>
-        {post.isGif && (
-          <div className="absolute right-2 top-2 grid h-10 w-10 place-items-center rounded-md bg-zinc-900">
-            GIF
-          </div>
-        )}
+        {post.isGif && <CornerInfo data="GIF" />}
       </Preview>
     );
     // } else if (post.type === "comment") {
     //   Post = <Comment permalink={post.permalink} comment={post.comment} />;
   } else {
-    Post = <div className="grid aspect-square place-items-center text-8xl">?</div>;
+    Post = <div className="grid aspect-square place-items-center bg-zinc-800 text-8xl">?</div>;
   }
 
   return (
-    <div className="rounded-md bg-zinc-800 p-2">
+    <div className="relative grid gap-1 overflow-hidden rounded-xl">
       <Details post={post} />
       {Post}
+    </div>
+  );
+}
+
+function CornerInfo({ data }: { data: string | number }) {
+  return (
+    <div className="absolute right-2 top-2 grid h-8 min-w-8 place-items-center rounded-md bg-zinc-900 px-2 font-semibold">
+      {data}
     </div>
   );
 }
