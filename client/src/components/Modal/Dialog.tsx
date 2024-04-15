958c992b1f1e8f438e8d15fee8330f5d7dd3fdb1
Maximtamgoo <maximtamgoo@gmail.com>
2024-04-15 13:25:40 -0400

    stop scrollbar layout shift when opening modal

--- client/src/components/Modal/Dialog.tsx
+++ client/src/components/Modal/Dialog.tsx
@@ -8,16 +8,23 @@ export default function Dialog({ onClose, children }: PropsWithChildren<Props>) 
   const ref = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
-    document.body.style.overflow = "hidden";
-    ref.current?.showModal();
-    return () => (document.body.style.overflow = "auto");
+    if (ref.current) {
+      const scrollbarWidth = window.innerWidth - document.body.clientWidth + "px";
+      document.body.style.paddingRight = scrollbarWidth;
+      document.body.style.overflow = "hidden";
+      ref.current.showModal();
+    }
+    return () => {
+      document.body.style.paddingRight = "0";
+      document.body.style.overflow = "auto";
+    };
   }, []);
 
   return createPortal(
     <dialog
       ref={ref}
       onClose={onClose}
-      className="h-screen max-h-full w-screen max-w-full bg-transparent"
+      className="h-full max-h-full w-full max-w-full bg-transparent"
     >
       {children}
     </dialog>,
