549508bbfe36db04f7a9c8872836a138f84bf9bf
Maximtamgoo <maximtamgoo@gmail.com>
2024-04-28 13:36:57 -0400

    card height constant

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,3 +1,5 @@
 export const ITEM_SIZE = 400;
 export const MIN_LANES = 1;
 export const MAX_LANES = 3;
+export const CARD_MIN_HEIGHT = "300px";
+export const CARD_MAX_HEIGHT = "calc(100vh - 12rem)";
