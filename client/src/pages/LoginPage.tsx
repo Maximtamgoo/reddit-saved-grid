1c09dbf78cc57554e6a15995c4ae0be23cd4381b
Maximtamgoo <maximtamgoo@gmail.com>
2024-05-28 17:34:12 -0400

    aligned bookmark svg

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -4,11 +4,11 @@ export default function LoginPage() {
   return (
     <div className="flex h-screen flex-col items-center gap-10 bg-slate-50 p-4 pt-32 text-center text-slate-700">
       <div className="text-4xl">
-        <Bookmark className="inline-block size-10 shrink-0 fill-sky-500 stroke-sky-500" />
+        <Bookmark className="inline-block size-10 fill-sky-500 stroke-sky-500 align-top" />
         Reddit Saved Masonry
       </div>
       <button
-        className="h-12 w-44 rounded-md bg-slate-200 hover:ring-2 hover:ring-slate-300"
+        className="h-12 w-44 shrink-0 rounded-md bg-slate-200 hover:ring-2 hover:ring-slate-300"
         onClick={() => (window.location.href = "/api/authurl")}
       >
         Login with Reddit
