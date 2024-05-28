5d7694c4d0f8bafcb1e72be3bf5bbeee30dfbc1d
Maximtamgoo <maximtamgoo@gmail.com>
2024-05-28 17:13:02 -0400

    plugin-react-swc

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import checker from "vite-plugin-checker";
 import { visualizer } from "rollup-plugin-visualizer";
