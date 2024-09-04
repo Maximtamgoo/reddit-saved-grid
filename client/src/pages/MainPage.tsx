a6892f06454f2e2a2dff92c18fde768859d3002d
Maximtamgoo <maximtamgoo@gmail.com>
2024-09-04 19:26:05 -0400

    if mobile use different maxHeight

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,10 +1,13 @@
 import Card from "@src/components/Card/Card";
+import Header from "@src/components/Header";
 import VirtualMasonry from "@src/components/VirtualMasonry";
 import { Post } from "@src/schema/Post";
 import { useGetSavedContent } from "@src/services/queries";
 import LoaderCircle from "@src/svg/loader-circle.svg?react";
 import { calculateAspectRatioFit } from "@src/utils/calculateAspectRatioFit";
 import { useCallback, useMemo, useRef } from "react";
+
+const isMaybeMobile = "maxTouchPoints" in navigator && navigator.maxTouchPoints > 0;
 
 export default function MainPage() {
   const isBusyRef = useRef(false);
@@ -13,17 +16,16 @@ export default function MainPage() {
   const posts = useMemo(() => data?.pages.flatMap((page) => page.posts) ?? [], [data]);
 
   const estimateSize = useCallback((item: Post, width: number) => {
-    const post = item;
     const detailsHeight = 96;
     const minHeight = 350;
-    const maxHeight = window.outerHeight - 100;
+    const maxHeight = isMaybeMobile ? window.outerHeight - 100 : window.innerHeight - 100;
 
-    if (post.type === "gallery" || post.type === "image") {
+    if (item.type === "gallery" || item.type === "image") {
       const ratioSize = calculateAspectRatioFit(
-        post.preview.width,
-        post.preview.height,
+        item.preview.width,
+        item.preview.height,
         width,
-        post.preview.height
+        item.preview.height
       );
       const ratioSizeHeight = Math.round(ratioSize.height);
       return Math.max(minHeight, Math.min(maxHeight, ratioSizeHeight + detailsHeight));
@@ -56,29 +58,32 @@ export default function MainPage() {
   }
 
   return (
-    <main className="bg-slate-50 text-slate-800">
-      <div className="m-auto max-w-screen-2xl px-2 py-5">
-        <VirtualMasonry
-          items={posts}
-          maxLanes={3}
-          laneWidth={350}
-          gap={20}
-          overscan={20}
-          getItemKey={getItemKey}
-          estimateSize={estimateSize}
-          loadMore={loadMore}
-          renderItem={(item) => <Card post={item} />}
-          renderLoader={
-            <div className="grid h-20 place-items-center text-xl">
-              {hasNextPage ? (
-                <LoaderCircle className="size-14 animate-spin" />
-              ) : (
-                "Reached the Reddit limit..."
-              )}
-            </div>
-          }
-        />
-      </div>
-    </main>
+    <>
+      <Header />
+      <main className="pt-16 text-slate-800">
+        <div className="m-auto max-w-screen-2xl px-2 py-5">
+          <VirtualMasonry
+            items={posts}
+            maxLanes={3}
+            maxLaneWidth={350}
+            gap={20}
+            overscan={20}
+            getItemKey={getItemKey}
+            estimateSize={estimateSize}
+            loadMore={loadMore}
+            renderItem={(item) => <Card post={item} />}
+            renderLoader={
+              <div className="grid h-20 place-items-center text-xl">
+                {hasNextPage ? (
+                  <LoaderCircle className="size-14 animate-spin" />
+                ) : (
+                  "Reached the Reddit limit..."
+                )}
+              </div>
+            }
+          />
+        </div>
+      </main>
+    </>
   );
 }
