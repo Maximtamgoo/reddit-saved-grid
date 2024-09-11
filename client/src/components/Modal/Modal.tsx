49e5ed45b7bf6f95bc56a56e410bf2996ce8acaf
Maximtamgoo <maximtamgoo@gmail.com>
2024-09-10 22:43:28 -0400

    add redirect link in modal

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,6 +1,8 @@
 import { Post } from "@src/schema/Post";
 import ArrowLeft from "@src/svg/arrow-left.svg?react";
+import ArrowRedirect from "@src/svg/square-arrow-out-up-right.svg?react";
 
+import Link from "../Link";
 import { Gallery } from "./Gallery";
 
 type Props = {
@@ -8,6 +10,7 @@ type Props = {
 };
 
 export default function Modal({ post }: Props) {
+  const subredditLink = `https://www.reddit.com/r/${post.subreddit}`;
   return (
     <div className="fixed inset-0 bg-slate-50">
       {post.type === "gallery" && <Gallery urls={post.gallery.map((e) => e.url)} />}
@@ -17,6 +20,12 @@ export default function Modal({ post }: Props) {
           <ArrowLeft />
         </button>
       </form>
+      <Link
+        className="absolute right-2 top-2 grid size-10 place-items-center rounded-full bg-slate-200 text-slate-800 hover:ring-2 hover:ring-slate-300"
+        href={subredditLink}
+      >
+        <ArrowRedirect />
+      </Link>
     </div>
   );
 }
